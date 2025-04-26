a4aeb15b1121bc0df8f361979a9825eec6b2fa87
Amanda <139074521+amandaalmeidadev@users.noreply.github.com>
2025-04-25 23:05:40 -0300

    Adicionando arquivos do projeto CadastroMidias

---
+++ CadastroMidias/src/Musica.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author amand
+ */
+public class Musica {
+    private String titulo;
+    private String artista;
+
+    public Musica(String titulo, String artista) {
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Título: " + titulo + " | Artista: " + artista;
+    }
+}
